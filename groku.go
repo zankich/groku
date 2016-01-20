8ad1f9ea1d019b16469a24b7f49259556a38f55c
Adrian Zankich <adzankich@gmail.com>
2016-01-19 23:01:52 -0800

    Rewrite and bug fixes

--- groku.go
+++ groku.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
-
-	"github.com/codegangsta/cli"
 )
 
-const VERSION = "0.3"
-
 var CONFIG string
+
+const (
+	VERSION = "0.4"
+	USAGE   = `usage: groku [--version] [--help] <command> [<args>]
+
+Commands:
+  home            Return to the home screen
+  rev             Reverse
+  fwd             Fast Forward
+  select          Select
+  left            Left
+  right           Right
+  up              Up
+  down            Down
+  back            Back
+  info            Info
+  backspace       Backspace
+  enter           Enter
+  search          Search
+  replay          Replay
+  play            Play
+  pause           Pause
+  discover        Discover a roku on your local network
+  text            Send text to the roku
+  apps            List installed apps on your roku
+  app             Launch specified app
+`
+)
 
 type dictonary struct {
 	XMLName xml.Name `xml:"apps"`
@@ -27,153 +53,172 @@ type app struct {
 	ID   string `xml:"id,attr"`
 }
 
+type grokuConfig struct {
+	Address   string `json:"address"`
+	Timestamp int64  `json:"timestamp"`
+}
+
 func main() {
-	CONFIG = fmt.Sprintf("%v/groku", os.TempDir())
-	app := cli.NewApp()
-	app.Name = "groku"
-	app.Version = VERSION
-	app.Usage = "roku CLI remote"
-	app.Commands = commands()
-	app.Run(os.Args)
+	CONFIG = fmt.Sprintf("%s/groku.json", os.TempDir())
+
+	if len(os.Args) == 1 || os.Args[1] == "--help" || os.Args[1] == "-help" ||
+		os.Args[1] == "--h" || os.Args[1] == "-h" || os.Args[1] == "help" {
+		fmt.Println(USAGE)
+		os.Exit(0)
+	}
+
+	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version" ||
+		os.Args[1] == "--version") {
+		fmt.Printf("groku version %s\n", VERSION)
+		os.Exit(0)
+	}
+
+	switch os.Args[1] {
+	case "home", "rev", "fwd", "select", "left", "right", "down", "up",
+		"back", "info", "backspace", "enter", "search":
+		http.PostForm(fmt.Sprintf("%vkeypress/%v", getRokuAddress(), os.Args[1]), nil)
+		os.Exit(0)
+	case "replay":
+		http.PostForm(fmt.Sprintf("%vkeypress/%v", getRokuAddress(), "InstantReplay"), nil)
+		os.Exit(0)
+	case "play", "pause":
+		http.PostForm(fmt.Sprintf("%vkeypress/%v", getRokuAddress(), "Play"), nil)
+		os.Exit(0)
+	case "discover":
+		fmt.Println("Found roku at", getRokuAddress())
+		os.Exit(0)
+	case "text":
+		if len(os.Args) < 3 {
+			fmt.Println(USAGE)
+			os.Exit(1)
+		}
+
+		roku := getRokuAddress()
+		for _, c := range os.Args[2] {
+			http.PostForm(fmt.Sprintf("%skeypress/Lit_%s", roku, url.QueryEscape(string(c))), nil)
+		}
+		os.Exit(0)
+	case "apps":
+		dict := queryApps()
+		for _, a := range dict.Apps {
+			fmt.Println(a.Name)
+		}
+		os.Exit(0)
+	case "app":
+		if len(os.Args) < 3 {
+			fmt.Println(USAGE)
+			os.Exit(1)
+		}
+
+		dict := queryApps()
+
+		for _, a := range dict.Apps {
+			if a.Name == os.Args[2] {
+				http.PostForm(fmt.Sprintf("%vlaunch/%v", getRokuAddress(), a.ID), nil)
+				os.Exit(0)
+			}
+		}
+		fmt.Printf("App %q not found\n", os.Args[2])
+		os.Exit(1)
+	default:
+		fmt.Println(USAGE)
+		os.Exit(1)
+	}
 }
 
 func queryApps() dictonary {
-	resp, _ := http.Get(fmt.Sprintf("%vquery/apps", findRoku()))
-	body := make([]byte, 2048)
-	n, _ := resp.Body.Read(body)
+	resp, err := http.Get(fmt.Sprintf("%squery/apps", getRokuAddress()))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
 
 	var dict dictonary
-	if err := xml.Unmarshal(body[:n], &dict); err != nil {
-		log.Fatalln(err)
+	if err := xml.NewDecoder(resp.Body).Decode(&dict); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
 	return dict
 }
 
 func findRoku() string {
-	fi, err := os.Open(CONFIG)
-	defer fi.Close()
+	ssdp, err := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
 	if err != nil {
-		ssdp, _ := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
-		addr, _ := net.ResolveUDPAddr("udp", ":0")
-		socket, _ := net.ListenUDP("udp", addr)
-
-		socket.WriteToUDP([]byte("M-SEARCH * HTTP/1.1\r\n"+
-			"HOST: 239.255.255.250:1900\r\n"+
-			"MAN: \"ssdp:discover\"\r\n"+
-			"ST: roku:ecp\r\n"+
-			"MX: 3 \r\n\r\n"), ssdp)
-
-		answerBytes := make([]byte, 1024)
-		socket.SetReadDeadline(time.Now().Add(3 * time.Second))
-		_, _, err := socket.ReadFromUDP(answerBytes[:])
-		if err != nil {
-			fmt.Println("Could not find your Roku!")
-			os.Exit(1)
-		}
-
-		ret := strings.Split(string(answerBytes), "\r\n")
-		location := strings.TrimPrefix(ret[len(ret)-3], "LOCATION: ")
-
-		fi, err := os.Create(CONFIG)
-		defer fi.Close()
-		if err != nil {
-			return location
-		}
-		fi.Write([]byte(location))
-		return location
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	buf := make([]byte, 1024)
-	n, err := fi.Read(buf[:])
+
+	addr, err := net.ResolveUDPAddr("udp", ":0")
 	if err != nil {
-		os.Remove(CONFIG)
-		return findRoku()
-	} else {
-		return string(buf[:n])
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	socket, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = socket.WriteToUDP([]byte("M-SEARCH * HTTP/1.1\r\n"+
+		"HOST: 239.255.255.250:1900\r\n"+
+		"MAN: \"ssdp:discover\"\r\n"+
+		"ST: roku:ecp\r\n"+
+		"MX: 3 \r\n\r\n"), ssdp)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	answerBytes := make([]byte, 1024)
+	err = socket.SetReadDeadline(time.Now().Add(3 * time.Second))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, _, err = socket.ReadFromUDP(answerBytes[:])
+	if err != nil {
+		fmt.Println("Could not find your Roku!")
+		os.Exit(1)
+	}
+
+	ret := strings.Split(string(answerBytes), "\r\n")
+	location := strings.TrimPrefix(ret[len(ret)-3], "LOCATION: ")
+
+	return location
 }
 
-func commands() []cli.Command {
-	cmds := []cli.Command{}
-	for _, cmd := range []string{
-		"home",
-		"rev",
-		"fwd",
-		"select",
-		"left",
-		"right",
-		"down",
-		"up",
-		"back",
-		"info",
-		"backspace",
-		"enter",
-		"search",
-	} {
-		cmds = append(cmds, cli.Command{
-			Name:  cmd,
-			Usage: cmd,
-			Action: func(c *cli.Context) {
-				http.PostForm(fmt.Sprintf("%vkeypress/%v", findRoku(), c.Command.Name), nil)
-			},
-		})
+func getRokuAddress() string {
+	var configFile *os.File
+	var config grokuConfig
+
+	configFile, err := os.Open(CONFIG)
+
+	// the config file doesn't exist, but that's okay
+	if err != nil {
+		config.Address = findRoku()
+		config.Timestamp = time.Now().Unix()
+	} else {
+		// the config file exists
+		if err := json.NewDecoder(configFile).Decode(&config); err != nil {
+			config.Address = findRoku()
+		}
+
+		//if the config file is over 60 seconds old, then replace it
+		if config.Timestamp == 0 || time.Now().Unix()-config.Timestamp > 60 {
+			config.Address = findRoku()
+			config.Timestamp = time.Now().Unix()
+		}
 	}
-	cmds = append(cmds, cli.Command{
-		Name:  "replay",
-		Usage: "replay",
-		Action: func(c *cli.Context) {
-			http.PostForm(fmt.Sprintf("%vkeypress/%v", findRoku(), "InstantReplay"), nil)
-		},
-	})
-	cmds = append(cmds, cli.Command{
-		Name:  "play",
-		Usage: "play/pause",
-		Action: func(c *cli.Context) {
-			http.PostForm(fmt.Sprintf("%vkeypress/%v", findRoku(), "Play"), nil)
-		},
-	})
-	cmds = append(cmds, cli.Command{
-		Name:  "discover",
-		Usage: "discover roku on your local network",
-		Action: func(c *cli.Context) {
-			os.Remove(CONFIG)
-			fmt.Println("Found roku at", findRoku())
-		},
-	})
-	cmds = append(cmds, cli.Command{
-		Name:  "text",
-		Usage: "send text to the roku",
-		Action: func(c *cli.Context) {
-			roku := findRoku()
-			for _, c := range c.Args()[0] {
-				http.PostForm(fmt.Sprintf("%vkeypress/Lit_%v", roku, string(c)), nil)
-			}
-		},
-	})
-	cmds = append(cmds, cli.Command{
-		Name:  "apps",
-		Usage: "list installed apps on roku",
-		Action: func(c *cli.Context) {
-			dict := queryApps()
-			fmt.Println("Installed apps:")
-			for _, a := range dict.Apps {
-				fmt.Println(a.Name)
-			}
-		},
-	})
-	cmds = append(cmds, cli.Command{
-		Name:  "app",
-		Usage: "launch specified app",
-		Action: func(c *cli.Context) {
-			dict := queryApps()
-			for _, a := range dict.Apps {
-				if a.Name == c.Args()[0] {
-					http.PostForm(fmt.Sprintf("%vlaunch/%v", findRoku(), a.ID), nil)
-					return
-				}
-			}
-			fmt.Println("App not found!")
-			os.Exit(1)
-		},
-	})
-	return cmds
+
+	if b, err := json.Marshal(config); err == nil {
+		ioutil.WriteFile(CONFIG, b, os.ModePerm)
+	}
+
+	return config.Address
 }
